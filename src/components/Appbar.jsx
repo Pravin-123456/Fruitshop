9f22b7bd17c36f7649c02845de91abb06c165636
Pravin <pravin709266@gmail.com>
2025-05-05 23:50:51 +0530

    change the file extention

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
 const Appbar = () => {
+  const [open, setOpen] = useState(false);
 
-  const [open,setOpen] = useState(false)
   const menuClicked = () => {
     console.log("menuClicked");
-    setOpen(!open)
+    setOpen(!open);
   };
 
   return (
@@ -37,36 +37,38 @@ const Appbar = () => {
         </div>
         <div className={`flex md:block justify-end ${open ? "block" : "hidden"}`}>
           <ul className="md:flex md:space-x-8 space-y-8 md:space-y-0">
-            <li className=" menu border-b-4">
-              <a href="#" className="font-body">
+            <li className="menu border-b-4">
+              <a href="/fruits" className="font-body">
                 Fruits
               </a>
             </li>
             <li className="menu">
-              <a href="#" className="font-body">
+              <a href="/vegetables" className="font-body">
                 Vegetables
               </a>
             </li>
             <li className="menu">
-              <a href="#" className="font-body">
+              <a href="/more" className="font-body">
                 More
               </a>
             </li>
             <li className="px-4 py-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 text-green-500"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                />
-              </svg>
+              <a href="/cart" aria-label="Shopping Cart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-green-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                  />
+                </svg>
+              </a>
             </li>
           </ul>
         </div>
